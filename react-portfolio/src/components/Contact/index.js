06612f2c6a068b1d16ce7915ca1e20214dc2b569
a-fash <andrew.fash@rutgers.edu>
2023-01-17 17:13:56 -0500

    Added emailjs functionality

--- react-portfolio/src/components/Contact/index.js
+++ react-portfolio/src/components/Contact/index.js
@@ -1,12 +1,14 @@
 import './index.scss'
 import Loader from 'react-loaders'
 import AnimatedLetters from '../AnimatedLetters'
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import { MapContainer, TileLayer, Marker } from 'react-leaflet'
+import emailjs from '@emailjs/browser'
 
 const Contact = () => {
 
     const [letterClass, setLetterClass] = useState('text-animate')
+    const form = useRef()
 
     useEffect(() => {
         setTimeout(() => {
@@ -14,10 +16,26 @@ const Contact = () => {
         }, 4000)
       }, [])
 
+    const sendEmail = (e) => {
+        e.preventDefault()
+    
+        emailjs
+          .sendForm('gmail', 'template_YeJhZkgb', form.current, '3rvnyctGKdrtOqC2y')
+          .then(
+            () => {
+              alert('Message successfully sent!')
+              window.location.reload(false)
+            },
+            () => {
+              alert('Failed to send the message, please try again')
+            }
+          )
+    }
+
     return (
      <>
-        <div className='container contact-page'>
-            <div className='text-zone'>
+        <div className="container contact-page">
+            <div className="text-zone">
                 <h1>
                     <AnimatedLetters
                         letterClass={letterClass}
@@ -28,13 +46,13 @@ const Contact = () => {
                 <p>
                     Filler
                 </p>
-                <div className='contact-form'>
-                    <form>
+                <div className="contact-form">
+                    <form ref={form} onSubmit={sendEmail}>
                         <ul>
-                            <li className='half'>
-                                <input type="text" name="name" placeholder='Name' required />
+                            <li className="half">
+                                <input type="text" name="name" placeholder="Name" required />
                             </li>
-                            <li className='half'>
+                            <li className="half">
                                 <input type="email" name="email" placeholder="Email" required />
                             </li>
                             <li>
@@ -44,7 +62,7 @@ const Contact = () => {
                                 <textarea name="message" placeholder="Message" required />
                             </li>
                             <li>
-                                <input type="submit" className='flat-button' value="SEND" />
+                                <input type="submit" className="flat-button" value="SEND" />
                             </li>
                         </ul>
                     </form>
